ef9489ffee6017fcf6e8e8f42d1f1081909de758
Justin Matheson <78773481+RumbleCA@users.noreply.github.com>
2021-02-14 15:52:49 -0600

    Add files via upload

---
+++ PlayerNames.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blothhoondr
+{
+    public class PlayerNames
+    {
+        static void Letters(int yep)
+        {
+            Random r = new Random();
+
+            string[] consonants = ("qu", "w", "r", "t", "p", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m");
+            string[] vowels = ("a", "e", "i", "o", "u", "y");
+            string name = "";
+
+            name += consonants[r.Next(consonants.Length)];
+            name += vowels[r.Next(vowels.Length)];
+
+            int b = 2;
+            while (b < yep) ;
+            {
+                name += consonants[r.Next(consonants.Length)];
+                b++;
+                name += vowels[r.Next(vowels.Length)];
+                b++;
+            }
+        }
+    }
+}
