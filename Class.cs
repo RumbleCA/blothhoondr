00773d4fe95fb03ca9db6e3a2c96603b2b105e3b
Justin Matheson <78773481+RumbleCA@users.noreply.github.com>
2021-02-14 13:08:58 -0600

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blothhoondr
+{
+    public class Scoreboard
+    {
+        static void GamesWon()
+        {
+            int wins;
+
+            Random win = new Random();
+            wins = win.Next(750, 17500);
+
+            int games;
+
+            Random game = new Random();
+            games = game.Next(1500, 20000);
+
+            int WL;
+
+            WL = wins / games;
+
+
+        }
+        static void TopTens()
+        {
+            int tops;
+
+            Random top = new Random();
+            tops = top.Next(1250, 18000);
+
+        }
+        static void DeathandKills()
+        {
+            int deaths;
+
+            Random death = new Random();
+            deaths = death.Next(1000, 12000);
+
+            int kills;
+
+            Random kill = new Random();
+            kills = kill.Next(1000, 25000);
+
+            int KD;
+
+            KD = kills / deaths;
+
+        }
+        
+
+        
+          
+            
+       
+
+        
+        
+        
+    }
+}
